8c9e69eafa0c499110fd2940b373bf907b284731
cristian pajoy <cpajoydiaz@gmail.com>
2023-10-19 19:58:21 -0500

    nuevo lista de contacto

--- lista.js
+++ lista.js
@@ -1,45 +1,66 @@
-let lista_contacto =[];
+const lista_Contacto = [];
 
-function contacto (id,nombre,apellido,edad,email,telefono,cuidad,direccion){
-    this.id = id,
-    this.nombre = nombre,
-    this.apellido = apellido,
-    this.edad = edad,
-    this.email = email,
-    this.telefono = telefono,
-    this.ubicacion = {
-        cuidad: cuidad,
-       direccion: direccion
-    }
-    
+ // nuevo Contacto
+ function Contacto (id, Nombre,Apellido,Edad, Email, Teléfono, ciudad, Direccion){ 
 
+        this.id = id;
+        this.Nombre = Nombre,
+        this.Apellido = Apellido,
+        this.Edad = Edad,
+        this.Email =Email,
+        this.Teléfono = Teléfono,
+        this.Ubicación = {
+                  ciudad: ciudad,
+                  Direccion: Direccion,
+                 }
+
+                
 }
 
-let contacto1 = new contacto (0,'cristian','pajoy',21,'cpajoydiaz@gmail.com',3004578934,'cartagena', 'Olaya Herrera, sector puntilla')
-lista_contacto.push(contacto1)
+  let contacto1 = new Contacto (0,'Elvin', 'Herrera',  29, 'elvinherrera16@gmail.com', 3002887944, 'Cartagena', 'Olaya Herrera, sector Playas Blancas')
+ lista_Contacto.push(contacto1)
 
-
-let contacto2 = new contacto (1,'kevin,gomez',30,'kevin1007@gmail.com',3014575934,'cartagena','Olaya Herrera, sector Playas Blancas')
-lista_contacto.push(contacto2)
-
-
-let contacto3 = new contacto (2,'maria','diaz',23,'diaz2002@gmail.com',3044578994,'cartagena','Olaya Herrera, sector stela')
-lista_contacto.push(contacto3)
-
-console.log(lista_contacto)
-
-
-function borrarContacto() {
-    const index = lista_contacto.indexOf
-    if (index !== -1) {
-        lista_contacto.splice(index, 1);
-       
-    } else {
-        console.log("El contacto no existe en la lista");
-    }
-  }
+ let contacto2 = new Contacto (1,'Kevin', 'Florez',  29, 'kevinflorez@gmail.com', 3005887944, 'Bolivar', 'La Estela sector Playas Blancas')
+ lista_Contacto.push(contacto2)
  
-  borrarContacto();
-  console.log(lista_contacto)
- 
+ let contacto3 = new Contacto (2,'Junior', 'Marin',  25, 'Jm@gmail.com', 3005587944, 'Sucre', ' sector Las Delicias')
+ lista_Contacto.push(contacto3)
+
+ console.log(lista_Contacto);
+
+
+ //Eliminar contacto(el Ultimo)
+function eliminarContacto(){
+   let removerContacto = [1];
+  lista_Contacto.pop(removerContacto)
   
+}
+eliminarContacto();
+console.log(lista_Contacto)
+
+ //imprimir los contactos presentes en la lista.
+
+
+ function imprimir (){ 
+
+  for(var i=0; i<lista_Contacto.length; i++){
+
+   }
+
+ }
+
+imprimir();
+console.log(lista_Contacto)
+
+
+ //actualizar contacto
+ function actualizarContacto(){
+let actualizar= lista_Contacto.findIndex((obj=> obj.id == 1));
+console.log("antes de actualizar: ", lista_Contacto[actualizar]);
+lista_Contacto[actualizar].Teléfono = 3001478855;
+lista_Contacto[actualizar].Ubicación.ciudad = 'San Andres';
+lista_Contacto[actualizar].Ubicación.Direccion= ' Las Proveedoras calle Brook Hill';
+console.log("Despues de actualizar : ", lista_Contacto[actualizar]); 
+ }
+ actualizarContacto();
+ 
